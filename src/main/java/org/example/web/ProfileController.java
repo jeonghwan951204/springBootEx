233023e8da5617e8c0e49c9a45d1289a871f9b39
jeonghwan951204 <wjdghks006@naver.com>
2022-11-16 01:18:08 +0900

    외부 설정 파일 가져오기 에러

--- src/main/java/org/example/web/ProfileController.java
+++ src/main/java/org/example/web/ProfileController.java
@@ -5,7 +5,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,7 +15,7 @@ public class ProfileController {
     private final Environment env;
 
     @GetMapping("/profile")
-    public String profile() throws SQLException {
+    public String profile() {
 
         // 현재 실행 중인 ActiveProfile을 모두 가져옴
         // real,oauth,real-db 등 활성화되어 있는 프로필을 가져옴
@@ -28,7 +27,12 @@ public class ProfileController {
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
         String profile = profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
         profiles.stream().forEach(sb::append);
-        sb.append(" "+ env.getProperty("spring.datasource.hikari.jdbc-url"));
+        if(env.getProperty("spring.datasource.hikari.jdbc-url") != null) {
+            sb.append(" "+ env.getProperty("spring.datasource.url"));
+        }
+        if(env.getProperty("spring.security.oauth2") != null) {
+            sb.append(env.getProperty("spring.security.oauth2"));
+        }
 
         return profile + " ======== " + sb.toString();
     }
