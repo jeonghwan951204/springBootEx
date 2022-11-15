51b01c20a436419119fa5fd0ad072e139e6897bc
ohjeonghwan <wjdghks006@naver.com>
2022-11-15 15:38:32 +0900

    switch.sh 서비스 url 안 바뀌는 에러 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,7 +11,7 @@ function switch_proxy() {
     echo "> Port 전환"
     # 하나의 문장을 만들어 파이프라인으로 넘겨주기 위해 echo를 사용, 엔진엑스가 변경할 프록시 주소 생성. 쌍따옴표를 사용하지 않으면 $servuce_url을 그대로 인식하지 못하고 변수를 찾음
     # 앞에서 넘겨준 문장을 service_url.inc에 덮어씀
-    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service_url.inc
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
     sudo service nginx reload
