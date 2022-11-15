30d5752f550865ab6ee78665ac6027c80e98c0f5
jeonghwan951204 <wjdghks006@naver.com>
2022-11-15 22:35:56 +0900

    import 파일 선언 변경, security 경로 수정

--- src/main/java/org/example/web/ProfileController.java
+++ src/main/java/org/example/web/ProfileController.java
@@ -5,6 +5,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,8 +18,10 @@ public class ProfileController {
 
     private final Environment env;
 
+    private final DataSource ds;
+
     @GetMapping("/profile")
-    public String profile() {
+    public String profile() throws SQLException {
 
         // 현재 실행 중인 ActiveProfile을 모두 가져옴
         // real,oauth,real-db 등 활성화되어 있는 프로필을 가져옴
@@ -24,7 +30,12 @@ public class ProfileController {
         List<String> realProfiles = Arrays.asList("real","real1","real2");
 
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        Connection con = ds.getConnection();
+        DatabaseMetaData dm = con.getMetaData();
 
-        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+        String db = env.getProperty(dm.getDatabaseProductName());
+        String profile = profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+
+        return db + " ====== " + profile;
     }
 }
