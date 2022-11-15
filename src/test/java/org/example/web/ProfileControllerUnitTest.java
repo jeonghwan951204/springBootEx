2d91101ecc98e5062781e8c9f0baf4b8f60e86c3
ohjeonghwan <wjdghks006@naver.com>
2022-11-15 11:05:02 +0900

    배포 시 포트 설정 api 추가

---
+++ src/test/java/org/example/web/ProfileControllerUnitTest.java
@@ -0,0 +1,46 @@
+package org.example.web;
+
+import org.example.web.ProfileController;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 스프링 환경이 필요하지 않기 때문에 springbootTest 없이 사용
+ */
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default_조회() {
+        // given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+}
