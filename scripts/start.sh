feff009203c459538300085189145cb4499cdfee
jeonghwan951204 <wjdghks006@naver.com>
2022-11-16 00:24:39 +0900

    외부 설정 파일 가져오기 에러

--- scripts/start.sh
+++ scripts/start.sh
@@ -24,12 +24,7 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-echo "nohup java -jar \
-            -Dspring.config.additional-location=file:/home/ec2-user/app/oauth.yml,file:/home/ec2-user/app/real-db.yml \
-            -Dspring.profiles.active=$IDLE_PROFILE \
-            $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &"
-
 nohup java -jar \
-      -Dspring.config.location=classpath:application-$IDLE_PROFILE.yml,file:/home/ec2-user/app/oauth.yml,file:/home/ec2-user/app/real-db.yml \
+      -Dspring.config.location=file:/home/ec2-user/app/oauth.yml,file:/home/ec2-user/app/real-db.yml \
       -Dspring.profiles.active=$IDLE_PROFILE \
       $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
