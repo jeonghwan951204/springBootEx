1b2b1abce934e97e99780c5fd41ae50893b48af1
jeonghwan951204 <wjdghks006@naver.com>
2022-11-16 02:12:26 +0900

    외부 설정 파일 가져오기 에러

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,9 +2,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 // spring security로 인해 controller 테스트가 안 되므로 주석 따로 분리
 //@EnableJpaAuditing      //JPA Auditing 활성화
+@ConstructorBinding
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
